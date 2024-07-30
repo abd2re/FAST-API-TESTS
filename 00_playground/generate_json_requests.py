bbf727359016db292c1ccfb1e65b407d6323262b
abd2re <awtcanada@gmail.com>
2024-07-30 17:49:53 -0400

    [fix] starting course

---
+++ 00_playground/generate_json_requests.py
@@ -0,0 +1,14 @@
+import json
+from uuid import uuid1
+from random import randint
+
+max_queries: int = 100
+
+for i in range(max_queries):
+    request: dict[str, str] = {
+        "id": str(uuid1()),
+        "ip_source": f"{randint(0,256)}.{randint(0,256)}.{randint(0,256)}.{randint(0,256)}",
+        "ip_destination": f"{randint(0,256)}.{randint(0,256)}.{randint(0,256)}.{randint(0,256)}",
+    }
+    with open(f"00_playground/json_requests/{i}.json", "w") as file:
+        file.write(json.dumps(request))
